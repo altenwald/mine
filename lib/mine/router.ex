63f311604e584c4eb77323b2bb1d52118a455287
Manuel Rubio <manuel@altenwald.com>
2019-06-13 17:57:49 +0100

    fix base URL to get index.html and information inside of index.html

--- lib/mine/router.ex
+++ lib/mine/router.ex
@@ -10,7 +10,8 @@ defmodule Mine.Router do
   plug :dispatch
 
   get "/" do
-    send_resp(conn, 200, "world")
+    priv_dir = :code.priv_dir(:mine)
+    send_file(conn, 200, "#{priv_dir}/static/index.html")
   end
 
   match _ do
