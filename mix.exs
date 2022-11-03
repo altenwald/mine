6e4401a85abe5e01ab65e219e72814a5627ed3e4
Manuel Rubio <manuel@altenwald.com>
2022-11-04 00:11:22 +0100

    perform the upgrade and run mix check

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Mine.MixProject do
       version: "0.6.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      preferred_cli_env: [
+        check: :test,
+        credo: :test,
+        dialyzer: :test,
+        doctor: :test
+      ]
     ]
   end
 
@@ -22,17 +29,33 @@ defmodule Mine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.1"},
-      {:plug_cowboy, "~> 2.0"},
-      {:uuid, "~> 1.1"},
-      {:etag_plug, "~> 0.2.0"},
-      {:ecto_mnesia, "~> 0.9.1"},
+      {:jason, "~> 1.4"},
+      {:plug, "~> 1.14"},
+      {:plug_cowboy, "~> 2.6"},
+      {:ecto_mnesia, github: "manuel-rubio/ecto_mnesia", branch: "support_for_ecto3"},
       {:number, "~> 1.0"},
       {:timex, "~> 3.5.0"},
 
       # for releases
       {:distillery, "~> 2.0"},
-      {:ecto_boot_migration, "~> 0.1.1"},
+      {:ecto_boot_migration, "~> 0.3"},
+
+      # tooling for quality check
+      {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:doctor, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:ex_check, "~> 0.14", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      check: [
+        "ecto.create",
+        "ecto.migrate",
+        "check"
+      ]
     ]
   end
 end
