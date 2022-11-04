c7afccc11d17c8cb387a4c86fc2ebdcfe2c7fd49
Manuel Rubio <manuel@altenwald.com>
2022-11-04 01:47:39 +0100

    fix dialyzer warning (an actual error)

--- lib/mine/websocket.ex
+++ lib/mine/websocket.ex
@@ -15,10 +15,7 @@ defmodule Mine.Websocket do
     remote_ip =
       case :cowboy_req.peer(req) do
         {{127, 0, 0, 1}, _} ->
-          case :cowboy_req.header("x-forwarded-for", req) do
-            {remote_ip, _} -> remote_ip
-            _ -> "127.0.0.1"
-          end
+          :cowboy_req.header("x-forwarded-for", req, "127.0.0.1")
 
         {remote_ip, _} ->
           to_string(:inet.ntoa(remote_ip))
